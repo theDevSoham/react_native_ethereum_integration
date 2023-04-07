0739e0646aa91c03e7c70228fb3fb007f0ab8f9b
Soham Das <publicsoham.24@gmail.com>
2023-04-07 22:24:40 +0530

    routing added

---
+++ screens/HomeScreen.tsx
@@ -0,0 +1,17 @@
+/* eslint-disable prettier/prettier */
+/* eslint-disable no-mixed-spaces-and-tabs */
+
+import { Text, View } from 'react-native';
+import React from 'react';
+
+const HomeScreen = () => {
+  return (
+	<View>
+	  <Text>HomeScreen</Text>
+	</View>
+  );
+};
+
+export default HomeScreen;
+
+//const styles = StyleSheet.create({});
---
+++ screens/SplashScreen.tsx
@@ -0,0 +1,17 @@
+/* eslint-disable prettier/prettier */
+/* eslint-disable no-mixed-spaces-and-tabs */
+
+import { Text, View } from 'react-native';
+import React from 'react';
+
+const SplashScreen = () => {
+  return (
+	<View>
+	  <Text>SplashScreen</Text>
+	</View>
+  );
+};
+
+export default SplashScreen;
+
+//const styles = StyleSheet.create({})
