c16e3c502638052e306eb0edda803633d59c748a
Soham Das <publicsoham.24@gmail.com>
2023-04-08 00:16:25 +0530

    home configuration started

--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -2,17 +2,31 @@
 /* eslint-disable eol-last */
 /* eslint-disable no-mixed-spaces-and-tabs */
 
-import { Text, View } from 'react-native';
+import { ImageBackground, StyleSheet, Text, View } from 'react-native';
 import React from 'react';
+import eth_bg from '../assets/images/eth_bg.jpg';
+import Overlay from '../components/Overlay';
 
 const HomeScreen = () => {
   return (
-	<View>
-	  <Text>HomeScreen</Text>
-	</View>
+	<ImageBackground source={eth_bg} style={styles.container}>
+		<Overlay opacity={0.6} />
+		<View style={styles.contentContainer}></View>
+	</ImageBackground>
   );
 };
 
 export default HomeScreen;
 
-//const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+	},
+
+	contentContainer: {
+		position: 'absolute',
+		width: '100%',
+		height: '100%',
+		zIndex: 2,
+	},
+});
